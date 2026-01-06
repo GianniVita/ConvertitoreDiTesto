067998961db4c109f2128a5909f17c324c5d3ff9
giannivitale <giavitale78@gmail.com>
2026-01-06 11:06:08 +0100

    feat: add Bootstrap for styling and enhance App component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 function App() {
 
   return (
-    <div>
-      <h1>Convertitore di Testo</h1>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">
+        Convertitore di Testo
+      </h1>
     </div>
+
+    <textarea 
+    className="form-controll mb-3" 
+    rows="4"></textarea>
 
   )
 
 }
+
+
+export default App
